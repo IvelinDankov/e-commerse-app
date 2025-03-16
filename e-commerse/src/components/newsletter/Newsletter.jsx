4fd1a74b654e351c19d21df08e5007e80f4669fd
Ivelin Dankov <you@example.com>
2025-03-16 09:35:57 +0200

    newsletter

--- e-commerse/src/components/newsletter/Newsletter.jsx
+++ e-commerse/src/components/newsletter/Newsletter.jsx
@@ -2,6 +2,8 @@
 import Button from "../ui/Button";
 import classes from "./Newsletter.module.css";
 
+import { Form } from "react-router-dom";
+
 import { SlEnvolope } from "react-icons/sl";
 
 
@@ -10,13 +12,13 @@ export default function Newsletter() {
         <div className={classes.newsletterWrapper}>
             <h3>Join Our Newsletter</h3>
             <p>Sign up for deals, new products and promotions</p>
-            <form action="" className={classes.newsletterForm}>
+            <Form action="" className={classes.newsletterForm}>
                 <p>
                     <SlEnvolope/>
                     <input type="email" />
                     <Button className={classes.button} textOnly>Sign up</Button>
                 </p>
-            </form>
+            </Form>
         </div>
     </section>
 }
